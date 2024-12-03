2097d9c03881ae186a9ec28f383bbf7b47cb382f
Iain05 <igriesdale@gmail.com>
2024-12-03 15:31:06 -0800

    chat we are graphing

--- graph.py
+++ graph.py
@@ -1,6 +1,18 @@
 import click
+import os
+import pandas as pd
+from uniplot import plot
 
 
 @click.command()
 def graph() -> None:
-    return
+    loadCSV(2024)
+
+
+def loadCSV(year) -> None:
+    target_path = os.path.join(os.path.dirname(__file__), (f"data/{year}.csv"))
+    headers = ["date", "mood", "energy", "ate", "tv", "hobbies", "games"]
+    data = pd.read_csv(target_path, sep=',', names=headers,
+                       usecols=["date", "mood"])
+    data = data.rename(columns={"day": "mood of the day"})
+    plot(xs=data.date, ys=data.mood, lines=True)
