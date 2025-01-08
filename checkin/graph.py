18579fe0cb368120ab43b9922cae3d1962ee8830
Iain05 <igriesdale@gmail.com>
2025-01-07 16:08:26 -0800

    fixed graph not getting current year

--- checkin/graph.py
+++ checkin/graph.py
@@ -4,14 +4,19 @@ import pandas as pd
 import plotext as plt
 from checkin.colorscheme import COLORS
 from checkin.config import *
+from datetime import datetime
+
+
+today = datetime.today()
 
 plt.date_form("Y-m-d")
 
 
 @click.command()
-def graph() -> None:
-    data = loadCSV(2024, "mood")
-    energy_data = loadCSV(2024, "energy")
+@click.option("-y", default=today.year, help="The year of data to graph")
+def graph(y) -> None:
+    data = loadCSV(y, "mood")
+    energy_data = loadCSV(y, "energy")
     plt.plot(data.date, data.mood, color=COLORS.red.rgb, marker="braille")
     plt.plot(data.date, energy_data.energy, marker="braille")
     plt.yticks(
@@ -29,7 +34,18 @@ def graph() -> None:
 
 def loadCSV(year, metric):
     target_path = os.path.join(DEFAULT_DIR, (f"data/{year}.csv"))
-    headers = ["date", "mood", "energy", "ate", "tv", "hobbies", "games", "cried"]
+    headers = [
+        "date",
+        "mood",
+        "energy",
+        "ate",
+        "tv",
+        "hobbies",
+        "games",
+        "cried",
+        "productive",
+        "sleep",
+    ]
     return pd.read_csv(
         filepath_or_buffer=target_path, sep=",", names=headers, usecols=["date", metric]
     )
